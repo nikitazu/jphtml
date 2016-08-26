923b80150e74a0b10d68e9aa82baf323043c2c10
Nikita Zuev <nikitazu@gmail.com>
2016-08-26 23:12:24 +0300

    Refactor

--- jphtml/Core/Html/HtmlPrinter.cs
+++ jphtml/Core/Html/HtmlPrinter.cs
@@ -1,14 +1,15 @@
 ﻿using System;
 using System.IO;
+using System.Xml.Linq;
 using jphtml.Core.Format;
 
 namespace jphtml.Core.Html
 {
-	public class HtmlPrinter
-	{
-		public void PrintDocumentBegin(TextWriter writer)
-		{
-			writer.WriteLine(@"<!DOCTYPE html>
+    public class HtmlPrinter
+    {
+        public void PrintDocumentBegin(TextWriter writer)
+        {
+            writer.WriteLine(@"<!DOCTYPE html>
 <html>
 <head>
   <title>JpHtml</title>
@@ -60,43 +61,52 @@ namespace jphtml.Core.Html
 
 <div style='display:inline-block;margin:0;padding:0;width:70%;border:1px solid red;'>
 ");
-		}
+        }
 
-		public void PrintDocumentEnd(TextWriter writer)
-		{
-			writer.WriteLine($@"
+        public void PrintDocumentEnd(TextWriter writer)
+        {
+            writer.WriteLine($@"
 </div>
 
 </body>
 </html>
 ");
-		}
+        }
 
-		public void PrintParagraphBegin(TextWriter writer)
-		{
-			writer.WriteLine("<p class='jp-text'>");
-		}
+        public void PrintParagraphBegin(TextWriter writer)
+        {
+            writer.WriteLine("<p class='jp-text'>");
+        }
 
-		public void PrintParagraphEnd(TextWriter writer)
-		{
-			writer.WriteLine("</p>");
-		}
+        public void PrintParagraphEnd(TextWriter writer)
+        {
+            writer.WriteLine("</p>");
+        }
 
-		public void PrintWord(TextWriter writer, WordInfo word)
-		{
-			var cssClass = EnumToCssClass(word.PartOfSpeech);
-			writer.WriteLine($"<span class='jp-part {cssClass}'>{RubyText(word)}{ContextHelp(word)}</span>");
-		}
+        public void PrintWord(TextWriter writer, WordInfo word)
+        {
+            var cssClass = EnumToCssClass(word.PartOfSpeech);
+            var part = Span(CssClass($"jp-part {cssClass}"), RubyWord(word), ContextHelp(word));
+            writer.WriteLine(part);
+        }
 
-		string RubyText(WordInfo word) =>
-			word.Text.Equals(word.Reading) ? 
-		        $"<ruby>{word.Text}</ruby>" : 
-		        $"<ruby>{word.Text}<rt>{word.Reading}</rt></ruby>";
+        XElement RubyWord(WordInfo word) =>
+            word.Text.Equals(word.Reading) || string.IsNullOrEmpty(word.Reading) ?
+                Ruby(word.Text) :
+                Ruby(word.Text, Rt(word.Reading));
 
-		string ContextHelp(WordInfo word) =>
-			$"<span class='jp-contexthelp'>k: {word.Text}<span>r: {word.Reading}</span><span>p: {word.Pronunciation}</span></span>";
+        XElement ContextHelp(WordInfo word) => Span(
+            CssClass("jp-contexthelp"),
+            Span("t: " + word.Text),
+            Span("r: " + word.Reading),
+            Span("p: " + word.Pronunciation));
 
-		string EnumToCssClass(Enum value) => $"jp-{value.ToString().ToLowerInvariant()}";
-	}
+        string EnumToCssClass(Enum value) => $"jp-{value.ToString().ToLowerInvariant()}";
+
+        XElement Span(params object[] content) => new XElement("span", content);
+        XElement Ruby(params object[] content) => new XElement("ruby", content);
+        XElement Rt(params object[] content) => new XElement("rt", content);
+        XAttribute CssClass(string value) => new XAttribute("class", value);
+    }
 }
 
