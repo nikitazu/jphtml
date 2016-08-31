225ddb5620d5402e9c1080902ca42961a76a906b
Nikita Zuev <nikitazu@gmail.com>
2016-09-01 01:14:13 +0300

    Extending Kana operations

--- jphtml/Core/Text/Kana.cs
+++ jphtml/Core/Text/Kana.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System;
+using System.Text.RegularExpressions;
 
 namespace jphtml.Core.Text
 {
@@ -7,9 +8,16 @@ namespace jphtml.Core.Text
     {
         const int DeltaKana = 'ア' - 'あ';
 
+        static Regex _hiraganaRegex = new Regex("^[ぁ-ゔゞ゛゜ー]+$", RegexOptions.Compiled);
+        static Regex _katakanaRegex = new Regex("^[ァ-・ヽヾ゛゜ー]+$", RegexOptions.Compiled);
+
         public static string HiraganaToKatakana(this string hiragana) => Convert(hiragana, c => (char)(c + DeltaKana));
 
         public static string KatakanaToHiragana(this string katakana) => Convert(katakana, c => (char)(c - DeltaKana));
+
+        public static bool IsHiragana(this string text) => _hiraganaRegex.IsMatch(text);
+
+        public static bool IsKatakana(this string text) => _katakanaRegex.IsMatch(text);
 
         static string Convert(string input, Func<char, char> conv) => new string(input.Select(conv).ToArray());
     }
