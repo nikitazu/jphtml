b7eb18f471e44938ded1cdb4bdd7cc05917b39f3
Nikita Zuev <nikitazu@gmail.com>
2016-09-01 01:12:53 +0300

    Unit testing WordInfo

---
+++ jphtml.Tests/Core/Format/WordInfoTest.cs
@@ -0,0 +1,52 @@
+﻿using jphtml.Core.Format;
+using NUnit.Framework;
+
+namespace jphtml.Tests.Core.Format
+{
+    [TestFixture]
+    public class WordInfoTest
+    {
+        [Test]
+        public void WordFuriganaShouldContainReadingWhenOnlyReading()
+        {
+            Assert.AreEqual("foo", new WordInfo { Reading = "foo" }.Furigana);
+        }
+
+        [Test]
+        public void WordFuriganaShouldContainPronunciationWhenOnlyPronunciation()
+        {
+            Assert.AreEqual("foo", new WordInfo { Pronunciation = "foo" }.Furigana);
+        }
+
+        [Test]
+        public void WordFuriganaShouldContainPronunciationWhenReadingAndPronunciation()
+        {
+            Assert.AreEqual("foo", new WordInfo { Pronunciation = "foo", Reading = "bar" }.Furigana);
+        }
+
+        [Test]
+        public void WordTextMaybeRootFormShouldContainRootFormWhenItIsPresent()
+        {
+            Assert.AreEqual("text (root)", new WordInfo { Text = "text", RootForm = "root" }.TextMaybeRootForm);
+        }
+
+        [Test]
+        public void WordTextMaybeRootFormShouldContainTextOnlyWhenRootFormIsAbsent()
+        {
+            Assert.AreEqual("text", new WordInfo { Text = "text" }.TextMaybeRootForm);
+        }
+
+        [Test]
+        public void WordTextMaybeRootFormShouldContainTextOnlyWhenTextIsInRootForm()
+        {
+            Assert.AreEqual("text", new WordInfo { Text = "text", RootForm = "text" }.TextMaybeRootForm);
+        }
+
+        [Test]
+        public void Foo()
+        {
+            Assert.AreEqual("Unknown,None,None,None", string.Join(",", new WordInfo().SpeechInfo));
+        }
+    }
+}
+
