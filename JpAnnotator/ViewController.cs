eb490e5d855da2f0f00cefa785dfec5b2aaaf04b
Nikita Zuev <nikitazu@gmail.com>
2016-09-05 23:03:05 +0300

    Mac app: counter

--- JpAnnotator/ViewController.cs
+++ JpAnnotator/ViewController.cs
@@ -3,17 +3,20 @@ using AppKit;
 using Foundation;
 using JpAnnotator.Logging;
 using JpAnnotator.Common.Mac;
+using JpAnnotator.Core;
 
 namespace JpAnnotator
 {
     public partial class ViewController : NSViewController
     {
+        Counter _counter;
         ILogWriter _log;
 
         public ViewController(IntPtr handle) : base(handle)
         {
             _log = new LoggingConfig(new MacResourceLocator()).CreateRootLogWriter();
             _log.Debug("start");
+            _counter = new Counter(_log);
         }
 
         public override void ViewDidLoad()
@@ -39,8 +42,9 @@ namespace JpAnnotator
 
         partial void ConvertButtonClicked(NSObject sender)
         {
-            _log.Error($"Convert {FileToConvert.StringValue}");
-            Console.WriteLine("console writes a line");
+            _log.Debug($"Convert {FileToConvert.StringValue}");
+            _counter.Start();
+            _counter.Stop();
         }
     }
 }
