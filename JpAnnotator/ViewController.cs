32c0e2492f3052fb9536e44d0175164dd28f3f47
Nikita Zuev <nikitazu@gmail.com>
2016-09-06 21:29:52 +0300

    Mac app: choose save target

--- JpAnnotator/ViewController.cs
+++ JpAnnotator/ViewController.cs
@@ -72,10 +72,55 @@ namespace JpAnnotator
         {
             _log.Debug($"Convert {FileToConvert.StringValue}");
 
+            var dlg = new NSSavePanel();
+            dlg.Title = "Save Text File";
+            dlg.AllowedFileTypes = new string[] { "epub" };
+            if (dlg.RunModal() == 1)
+            {
+
+            }
+            else
+            {
+                var alert = new NSAlert()
+                {
+                    AlertStyle = NSAlertStyle.Informational,
+                    InformativeText = "Conversion is cancelled because the target file wasn't chosen",
+                    MessageText = "Target file wasn't chosen",
+                };
+                alert.RunModal();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(FileToConvert.StringValue))
+            {
+                var alert = new NSAlert()
+                {
+                    AlertStyle = NSAlertStyle.Informational,
+                    InformativeText = "Conversion is cancelled because the source file wasn't chosen",
+                    MessageText = "Source file wasn't chosen",
+                };
+                alert.RunModal();
+                return;
+            }
+
+            if (!File.Exists(FileToConvert.StringValue))
+            {
+                var alert = new NSAlert()
+                {
+                    AlertStyle = NSAlertStyle.Informational,
+                    InformativeText = "Conversion is cancelled because the source file doesn't exist",
+                    MessageText = "Source file doesn't exist",
+                };
+                alert.RunModal();
+                return;
+            }
+
+            var outputDir = Path.GetDirectoryName(dlg.Url.Path);
+
             var options = new Options(new string[]
             {
                 "--inputFile",  FileToConvert.StringValue,
-                "--outputDir", Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "tmp"),
+                "--outputDir", Path.Combine(outputDir, Path.GetFileNameWithoutExtension(FileToConvert.StringValue)),
                 "--chapterMarkers", "第1章,第2章,第3章,第4章,第5章,第6章,第7章,第8章,第9章,第10章,第11章,第12章,第13章,第14章,第15章,第16章,第17章,第18章,第19章,第20章,第21章,第22章,第23章,第24"
             });
 
