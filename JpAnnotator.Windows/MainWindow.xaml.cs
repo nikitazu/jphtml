20af690a205aac3ed35f99a85af4c09718de279f
Nikita Zuev <nikitazu@gmail.com>
2016-09-14 19:38:01 +0300

    Windows app: implemented epub conversion
    closes #3

--- JpAnnotator.Windows/MainWindow.xaml.cs
+++ JpAnnotator.Windows/MainWindow.xaml.cs
@@ -1,7 +1,16 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Windows;
+using JpAnnotator.Common.Portable.Configuration;
 using JpAnnotator.Common.Portable.Gui;
+using JpAnnotator.Common.Portable.PlainText;
+using JpAnnotator.Common.Windows;
 using JpAnnotator.Common.Windows.Gui;
+using JpAnnotator.Core;
+using JpAnnotator.Core.Dic;
+using JpAnnotator.Core.Make.Epub;
+using JpAnnotator.Core.Make.Html;
+using JpAnnotator.Logging;
 
 namespace JpAnnotator.Windows
 {
@@ -19,7 +28,7 @@ namespace JpAnnotator.Windows
             _dialog = new WpfDialogCreator(this);
         }
 
-        private void OpenButtonClick(object sender, RoutedEventArgs e)
+        void OpenButtonClick(object sender, RoutedEventArgs e)
         {
             string path;
             if (_dialog.OpenFile("Choose file to convert", "Text files|*.txt|All Files|*.*", out path))
@@ -29,7 +38,7 @@ namespace JpAnnotator.Windows
             }
         }
 
-        private void ConvertButtonClick(object sender, RoutedEventArgs e)
+        async void ConvertButtonClick(object sender, RoutedEventArgs e)
         {
             if (string.IsNullOrWhiteSpace(_sourceFile))
             {
@@ -43,14 +52,48 @@ namespace JpAnnotator.Windows
                 return;
             }
 
-            string path;
-            if (!_dialog.SaveFile("Choose file to save", "Epub files|*.epub|All Files|*.*", _sourceFile + ".epub", out path))
+            string targetFile;
+            if (!_dialog.SaveFile("Choose file to save", "Epub files|*.epub|All Files|*.*", _sourceFile + ".epub", out targetFile))
             {
                 _dialog.Info("Target file not chosen", "Choose target file to convert");
                 return;
             }
 
-            _dialog.Info("TODO", $"Converting {_sourceFile} to {path}");
+            _dialog.Info("TODO", $"Converting {_sourceFile} to {targetFile}");
+
+            var resourceLocator = new WindowsResourceLocator();
+            var log = new LoggingConfig(resourceLocator).CreateRootLogWriter();
+            log.Debug("start");
+
+            var options = new Options(new string[] {
+                "--inputFile", _sourceFile,
+                "--outputFile", targetFile
+            });
+
+            var _htmlToEpub = new HtmlToEpubConverter(
+                new Counter(log),
+                log,
+                options,
+                new MecabParser(),
+                new MecabReader(),
+                new MecabBackend(),
+                new XHtmlMaker(),
+                new JmdicFastReader(
+                    log,
+                    resourceLocator,
+                    new Jmdictionary()
+                ),
+                new ContentsBreaker(new ChapterMarkersProvider(options, new ContentsDetector())),
+                new EpubMaker(log, options, resourceLocator),
+                new SentenceBreaker()
+            );
+
+            options.Print(Console.Out);
+
+            await _htmlToEpub.Convert().ContinueWith(_ =>
+            {
+                log.Debug("end");
+            });
         }
     }
 }
