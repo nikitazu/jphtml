263ecce2015b5eb57216cdcca226c3c73afaed2d
Nikita Zuev <nikitazu@gmail.com>
2016-09-14 14:04:16 +0300

    Windows - GUI app skeleton
    #3

---
+++ JpAnnotator.Windows/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace JpAnnotator.Windows
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenButtonClick(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ConvertButtonClick(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
