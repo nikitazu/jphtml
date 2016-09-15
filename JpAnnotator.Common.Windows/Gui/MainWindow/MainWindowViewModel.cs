8ff21e61d3e84ffa41dbef162c05ec00a6fd51f4
Nikita Zuev <nikitazu@gmail.com>
2016-09-15 22:59:48 +0300

    Windows app: use Fody for mvvm

--- JpAnnotator.Common.Windows/Gui/MainWindow/MainWindowViewModel.cs
+++ JpAnnotator.Common.Windows/Gui/MainWindow/MainWindowViewModel.cs
@@ -1,39 +1,12 @@
-﻿using System.ComponentModel;
-using JpAnnotator.Common.Portable.Gui.MainWindow;
+﻿using JpAnnotator.Common.Portable.Gui.MainWindow;
+using PropertyChanged;
 
 namespace JpAnnotator.Common.Windows.Gui.MainWindow
 {
-    public class MainWindowViewModel : IMainWindowViewModel, INotifyPropertyChanged
+    [ImplementPropertyChanged]
+    public class MainWindowViewModel : IMainWindowViewModel
     {
-        private string _sourceFile;
-        private string _targetFile;
-
-        public string SourceFile
-        {
-            get { return _sourceFile; }
-            set
-            {
-                if (_sourceFile != value)
-                {
-                    _sourceFile = value;
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SourceFile)));
-                }
-            }
-        }
-
-        public string TargetFile
-        {
-            get { return _targetFile; }
-            set
-            {
-                if (_targetFile != value)
-                {
-                    _targetFile = value;
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TargetFile)));
-                }
-            }
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
+        public string SourceFile { get; set; }
+        public string TargetFile { get; set; }
     }
 }
